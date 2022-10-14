cb94c0e5fb689132642f6474940aacada17964d0
TotaLukem <nickola.anelka@gmail.com>
2022-10-14 11:26:02 +0300

    feat: finish task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alph = [
+    'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ]
+
+  encrypt(message, key) {
+    try {
+      key = key.padEnd(message.length, key).toUpperCase().split('');
+      message = message.toUpperCase().split('');
+      message.forEach((elem, index) => {
+        if (this.alph.indexOf(elem) === -1) {
+          key.splice(index, 0, ' ');
+        }
+      });
+      key = key.splice(0, message.length);
+      message = message.map((elem, index) => {
+        if (this.alph.indexOf(elem) !== -1) {
+          let curIndex = this.alph.indexOf(elem) + this.alph.indexOf(key[index]);
+          if (curIndex > 25) {
+            return this.alph[curIndex - 26];
+          } else {
+            return this.alph[curIndex];
+          }
+        } else {
+          return elem;
+        }
+      })
+      return this.type === true ? message.join('') : message.reverse().join('');
+    } catch {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    try {
+      key = key.padEnd(encryptedMessage.length, key).toUpperCase().split('');
+      encryptedMessage = encryptedMessage.toUpperCase().split('');
+      encryptedMessage.forEach((elem, index) => {
+        if (this.alph.indexOf(elem) === -1) {
+          key.splice(index, 0, ' ');
+        }
+      });
+      key = key.splice(0, encryptedMessage.length);
+      encryptedMessage = encryptedMessage.map((elem, index) => {
+        if (this.alph.indexOf(elem) !== -1) {
+          let curIndex = this.alph.indexOf(elem) - this.alph.indexOf(key[index]);
+          if (curIndex < 0) {
+            return this.alph[this.alph.indexOf(elem) + 26 - this.alph.indexOf(key[index])];
+          } else {
+            return this.alph[this.alph.indexOf(elem) - this.alph.indexOf(key[index])];
+          }
+        } else {
+          return elem;
+        }
+      })
+      return this.type === true ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+    } catch {
+      throw new Error('Incorrect arguments!');
+    }
+
   }
 }
 
